8c3ad7ffd26a22a27f1d91121faeccd7e60d55f4
Ellie Richards <eleanor4.richards@live.uwe.ac.uk>
2018-02-03 13:10:41 +0000

    Not fake-working any more
    WB

--- Assets/Completed/Scripts - Map Gen/MapFill.cs
+++ Assets/Completed/Scripts - Map Gen/MapFill.cs
@@ -4,13 +4,148 @@ using UnityEngine;
 
 public class MapFill : MonoBehaviour
 {
+    private ObjectPool pool;
+    public int width = 0;
+    public int height = 0;
+
+    [Range(0, 10)]
+    public int smoothIterations = 0;
+
+    public int seed = 0;
+    public bool Cave;
+    private int[,] map;
+
+    [Range(0, 100)]
+    public int randomFillPercent;
+
     // Use this for initialization
-    void Start()
+    private void Start()
     {
+        pool = GameObject.Find("TilePooler").GetComponent<ObjectPool>();
+        //GenerateMap();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        if (Input.GetButton("Jump"))
+        {
+            GenerateMap();
+            Debug.Log("new map...");
+        }
+    }
+
+    private void DrawMapTiles()
+    {
+        //Find all active tiles
+        //_tile.SetActive(false);
+        ///Destroy(_tile);
+        pool.DeactivateObject("Tile");
+        pool.DeactivateObject("Blank");
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                if (map[x, y] == 1)
+                {
+                    GameObject tile = pool.GetPooledObject("Tile");
+                    tile.transform.position = new Vector2(x, y);
+                    tile.SetActive(true);
+                }
+                else if (map[x, y] == 0)
+                {
+                    GameObject blank = pool.GetPooledObject("Blank");
+                    blank.transform.position = new Vector2(x, y);
+                    blank.SetActive(true);
+                }
+            }
+        }
+    }
+
+    private void GenerateMap()
+    {
+        map = new int[width, height];
+        RandomFillMap();
+        for (int i = 0; i < smoothIterations; i++)
+        {
+            map = SmoothMap();
+        }
+        DrawMapTiles();
+    }
+
+    private void RandomFillMap()
+    {
+        System.Random rndSeed = new System.Random(seed);
+        if (Cave)
+        {
+        }
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                if (x == 0 || x == width - 1 || y == 0 || y == height - 1)
+                {
+                    map[x, y] = 1;
+                }
+                else
+                {
+                    map[x, y] = rndSeed.Next(0, 100);
+                    if (map[x, y] < randomFillPercent)
+                    {
+                        map[x, y] = 1;
+                    }
+                    else
+                    {
+                        map[x, y] = 0;
+                    }
+                }
+            }
+        }
+    }
+
+    private int[,] SmoothMap()
+    {
+        int[,] tempMap = new int[width, height];
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                int neighbouringWalls = GetSurroundingObjCount(x, y);
+
+                if (neighbouringWalls > 4)
+                {
+                    tempMap[x, y] = 1;
+                }
+                else
+                {
+                    tempMap[x, y] = 0;
+                }
+            }
+        }
+        return tempMap;
+    }
+
+    private int GetSurroundingObjCount(int curX, int curY)
+    {
+        int wallCount = 0;
+        for (int neighX = curX - 1; neighX <= curX + 1; neighX++)
+        {
+            for (int neighY = curY - 1; neighY <= curY + 1; neighY++)
+            {
+                if (neighX >= 0 && neighX < width && neighY >= 0 && neighY < height) //check within bounds
+                {
+                    if (neighX != curX || neighY != curY)
+                    {
+                        wallCount += map[neighX, neighY];
+                    }
+                }
+                else
+                {
+                    wallCount++;
+                }
+            }
+        }
+        return wallCount;
     }
 }
--- Assets/Completed/Scripts - Map Gen/ObjectPool.cs
+++ Assets/Completed/Scripts - Map Gen/ObjectPool.cs
@@ -21,19 +21,6 @@ public class ObjectPool : MonoBehaviour
     public List<ObjectPoolItem> itemsToPool;
     public static ObjectPool SharedInstance;
 
-    public int width = 0;
-    public int height = 0;
-
-    [Range(0, 10)]
-    public int smoothIterations = 0;
-
-    public int seed = 0;
-    public bool Cave;
-    private int[,] map;
-
-    [Range(0, 100)]
-    public int randomFillPercent;
-
     private void Awake()
     {
         SharedInstance = this;
@@ -56,128 +43,7 @@ public class ObjectPool : MonoBehaviour
                 pooledObject.Add(obj);
             }
         }
-        GenerateMap();
-    }
-
-    private void Update()
-    {
-        if (Input.GetButton("Jump"))
-        {
-            GenerateMap();
-            Debug.Log("new map...");
-        }
-    }
-
-    private void DrawMapTiles()
-    {
-        //Find all active tiles
-        //            _tile.SetActive(false);
-        //Destroy(_tile);
-        for (int x = 0; x < width; x++)
-        {
-            for (int y = 0; y < height; y++)
-            {
-                if (map[x, y] == 1)
-                {
-                    GameObject tile = GetPooledObject("Tile");
-                    tile.transform.position = new Vector2(x, y);
-                    tile.SetActive(true);
-                }
-                else if (map[x, y] == 0)
-                {
-                    GameObject blank = GetPooledObject("Blank");
-                    blank.transform.position = new Vector2(x, y);
-                    blank.SetActive(true);
-                }
-            }
-        }
-    }
-
-    private void GenerateMap()
-    {
-        map = new int[width, height];
-        RandomFillMap();
-        for (int i = 0; i < smoothIterations; i++)
-        {
-            map = SmoothMap();
-        }
-        DrawMapTiles();
-    }
-
-    private void RandomFillMap()
-    {
-        System.Random rndSeed = new System.Random(seed);
-        if (Cave)
-        {
-        }
-        for (int x = 0; x < width; x++)
-        {
-            for (int y = 0; y < height; y++)
-            {
-                if (x == 0 || x == width - 1 || y == 0 || y == height - 1)
-                {
-                    map[x, y] = 1;
-                }
-                else
-                {
-                    map[x, y] = rndSeed.Next(0, 100);
-                    if (map[x, y] < randomFillPercent)
-                    {
-                        map[x, y] = 1;
-                    }
-                    else
-                    {
-                        map[x, y] = 0;
-                    }
-                }
-            }
-        }
-    }
-
-    private int[,] SmoothMap()
-    {
-        int[,] tempMap = new int[width, height];
-
-        for (int x = 0; x < width; x++)
-        {
-            for (int y = 0; y < height; y++)
-            {
-                int neighbouringWalls = GetSurroundingObjCount(x, y);
-
-                if (neighbouringWalls > 4)
-                {
-                    tempMap[x, y] = 1;
-                }
-                else
-                {
-                    tempMap[x, y] = 0;
-                }
-            }
-        }
-        return tempMap;
-    }
-
-    private int GetSurroundingObjCount(int curX, int curY)
-    {
-        int wallCount = 0;
-        for (int neighX = curX - 1; neighX <= curX + 1; neighX++)
-        {
-            for (int neighY = curY - 1; neighY <= curY + 1; neighY++)
-            {
-                if (neighX >= 0 && neighX < width && neighY >= 0 && neighY < height) //check within bounds
-                {
-                    if (neighX != curX || neighY != curY)
-                    {
-                        wallCount += map[neighX, neighY];
-                    }
-                }
-                else
-                {
-                    wallCount++;
-                }
-            }
-        }
-        return wallCount;
+        //GenerateMap();
     }
 
     //Return inactive tiles in the pool
@@ -206,24 +72,14 @@ public class ObjectPool : MonoBehaviour
         return null;
     }
 
-    //OnCollisionExit (attached to player) disable tile
-    public void DisableOnExit(GameObject _tile)
+    public void DeactivateObject(string tag)
     {
-        if (_tile.tag == "Tile")
+        for (int i = 0; i < pooledObject.Count; i++)
         {
-            _tile.SetActive(false);
-        }
-        else
-        {
-            Destroy(_tile);
-        }
-    }
-
-    public void EnableOnEnter(GameObject _tile)
-    {
-        if (_tile.tag == "Tile")
-        {
-            //    _tile.SetActive(true);
+            if (pooledObject[i].activeInHierarchy && pooledObject[i].CompareTag(tag))
+            {
+                pooledObject[i].SetActive(false);
+            }
         }
     }
 }
